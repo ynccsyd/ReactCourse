30c8c21a1a037022585767d01e943d3bb834fb13
ynccsyd <muhibbi_10_@hotmail.com>
2023-01-26 14:16:43 +0300

    Added DOM

---
+++ Javascript/08DOM.js
@@ -0,0 +1,7 @@
+// DOM
+// https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model
+// pathname
+console. log(document. location.pathname)
+console.log(document.head)
+console.log(document.body)
+document.body.style.backgroundColor = "aqua"
