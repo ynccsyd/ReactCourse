e3bebc1fcc3fda560f278cfe0ac0adcf058011d2
ynccsyd <muhibbi_10_@hotmail.com>
2023-01-26 12:10:51 +0300

    Added string data

--- Javascript/07StringVeriTürü.js
+++ Javascript/07StringVeriTürü.js
@@ -18,3 +18,44 @@ console.log(firstName) //HAKAN
 firstName=firstName.toLowerCase() 
 console.log(firstName)//hakan
 
+// // String Icinde Istedigimiz Bilgi Aramak ve Yerini Bulma -> search:
+console.log(email.search("@")) //15
+console.log( email[15] ) // @
+email.search('olmayan') // olmayan bir dğer varsa -1 gösterir
+
+// belli bir yere kadar al -> slice : (domain bilgisi)
+let DOMAIN = email.slice( email.search("@") + 1) // gmail yazar. @ işaretinden sonrasını alır
+console. log(DOMAIN)
+console.log(
+DOMAIN. slice(0, DOMAIN.indexOf('.') ))// sadece gmail kismini aldik
+
+// bilgiyi degistir -> replace :
+email = email.replace(' gmail.com ', ' kodluyoruz.org ');
+console.log(email)
+
+// istedigim bilgi var mi ? -> includes :
+email.includes('dkfhsd') // false
+email.includes('@') // true
+
+// // istedigim bilgiler basladimi ? bitti mi -> startswith, endswith :
+console.log(
+    email.endsWith(' kodluyoruz.org ')
+)
+
+// Ilk Harflerini Buyuk Yapmak
+firstName = "FIRST"
+lastName = "LAST"
+let fullName =`${firstName[0].toUpperCase()}${firstName.slice(1).toLowerCase()} ${lastName[0].toUpperCase()}${lastName.slice(1).toLowerCase()}}`
+
+//Metin birleştirme
+console.log(firstName.concat('', lastName))
+
+//Metni diziye çevirme
+var isim ="Ali, Veli"
+var isimler= isim.split(',')
+console.log(isimler) // ['Ali', 'Veli']
+
+//charCodeAt – İndex Numarasına Göre Karakterin Unicode Değerini Bulma
+var cisim='ağaç';
+console.log(cisim.charCodeAt(1))// ğ=287
+
