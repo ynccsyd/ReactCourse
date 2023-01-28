88a4ba394a92c479d097fd9b34cf874591265de9
ynccsyd <muhibbi_10_@hotmail.com>
2023-01-28 13:10:02 +0300

    Added counter

--- React/react-practice/src/App.js
+++ React/react-practice/src/App.js
@@ -4,7 +4,7 @@ import Counter from "./components/Counter";
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <Counter/>
     </div>
   );
 }
--- React/react-practice/src/components/Counter.js
+++ React/react-practice/src/components/Counter.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useState } from 'react'
+
+
 
 const Counter = () => {
+  const [count, setCount]=useState(0);
+  // const increase =()=>{
+  //   setCount{count+1}
+  // }
+  // const decrease =()=>{
+  //   setCount{count-1}
+  // }
+
   return (
     <div>
-  
+      <h1>{count}</h1>
+      <button onClick={ ()=> setCount(count -1 ) }>Decrease</button>
+      <button onClick={ ()=> setCount(count + 1 ) }>Increase</button>
+    
+    
     </div>
   )
 }
