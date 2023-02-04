8238b49ab8ebdb5573cfc0fae8b5b4dae3191852
ynccsyd <muhibbi_10_@hotmail.com>
2023-02-04 17:01:06 +0300

    Added !=

--- Javascript/11ComparisonOperators .js
+++ Javascript/11ComparisonOperators .js
@@ -10,8 +10,20 @@ console.log(price===1); //false
 console.log(price===100); //true
 
 // != Eşit değilse
-console.log(user!="guest")
+console.log(user!="guest") //false
+console.log(user!="Ali") //true
+
 // < Küçükse
+console.log("price<100" , price<100)//false
 // <= Küçük veya eşitse
+
+console.log("price<=100" , price<=100)//true
 // > Büyükse
+console.log("price>200" , price>200)//false
 // >= Büyük veya eşitse
+
+//&& ve
+price=0
+console.log(price>0  && user!="guest") //false
+//|| veya
+console.log(price>0  || user!="guest") //true
