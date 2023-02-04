b3190af8f641114947c67a157b3db1c34e527323
ynccsyd <muhibbi_10_@hotmail.com>
2023-02-04 20:42:54 +0300

    Added Multi-Condition

---
+++ Javascript/13Multi-Condition.js
@@ -0,0 +1,12 @@
+//**-Coklu-Kosullarla-Calismak-**
+let userName = prompt("Kullanici Adiniz:")
+let age=prompt("Yasiniz")
+
+let info=document.querySelector("#info")
+if (userName && age >= 18){
+    info.innerhtml = "ehliyet alabilirsiniz"
+} else if (!username){
+    info.innerHTML = "Kullanici Adiniz Yok"
+} else if ( !(age > 18)){
+    info.innerHTML = "Yas Bilginiz Yok veya 18 Yasindan Kucuksunuz"
+}   
