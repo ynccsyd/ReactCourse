bbe2b67180f52563920df131898bd91dbcc5cf69
ynccsyd <muhibbi_10_@hotmail.com>
2023-02-04 14:14:21 +0300

    Added equality

---
+++ Javascript/11ComparisonOperators .js
@@ -0,0 +1,15 @@
+let price =100
+
+// == Eşitse
+Console.log( price ==1) //false
+console. log( price ==100) // true
+
+// === Hem değeri hem de türü eşitse
+console.log(price===1); //false
+console.log(price===100); //true
+
+// != Eşit değilse
+// < Küçükse
+// <= Küçük veya eşitse
+// > Büyükse
+// >= Büyük veya eşitse
