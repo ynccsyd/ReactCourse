df0306b24d880d010be37e48b8345438dc55d09d
ynccsyd <muhibbi_10_@hotmail.com>
2023-02-05 17:30:55 +0300

    Added Dom events

---
+++ Javascript/18DomEvents.js
@@ -0,0 +1,10 @@
+// DOM Etkinlikleriyle Calismak
+// https://www.w3schools.com/jsref/dom_obj_event.asp
+// https://developer.mozilla.org/en-US/docs/Web/Events
+let greeting = document.querySelector("#greeting")
+greeting.addEventListener("mouseover", domClick)
+
+function doclick() {
+    console.log("etkinlik denetlendi")
+    this.style.color == "red" ? this.style.color = "black" : this.style.color = "red"
+}
