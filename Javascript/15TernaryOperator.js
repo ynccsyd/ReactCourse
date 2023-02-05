9601aac143e04d7c37f17df6489d3ad92fca7c00
ynccsyd <muhibbi_10_@hotmail.com>
2023-02-05 09:10:06 +0300

    Added Ternary with chaining

--- Javascript/15TernaryOperator.js
+++ Javascript/15TernaryOperator.js
@@ -6,3 +6,12 @@ let info = document.querySelector("#info")
 // kosul ? dogruysa : yanlissa
 // userName.length > 0 ? userName : "Kullani Bilginiz Bulunamadi : ("]
 info.innerHTML = `${userName.length > 0 ? userName  : "Kullanici Bilginiz Bulunamadi :( "}`
+
+// Ternary Operatorleri Zincirleme
+var money = 40;
+var canBuy = 
+    (money < 17) ? "Satin alamazsin..":
+    (money > 30) ? "Satin alabilirsin..":
+    "Para miktarini girmen gerekmektedir..";
+
+console.log(canBuy) // "Satın alabilirsin.."
