228e8315cca9f73d90d9de03c8f584f52cba53dd
ynccsyd <muhibbi_10_@hotmail.com>
2023-03-17 20:59:27 +0300

    Added array

--- Javascript/22Array/arrayExercise.js
+++ Javascript/22Array/arrayExercise.js
@@ -16,21 +16,33 @@ console.log("items: ", items) // (3) [Array(3), 5, Array(3)]
 
 // Array icerisindeki ogenin index bilgisini bulmak -> indexOf('value')
 
-items. unshift(" lorem")
-items. push("ipsum")
-console.log( items.index0f("ipsum"))
+items.unshift(" lorem")
+items.push("ipsum")
+console.log( items.indexOf("ipsum")) //4
 
 //Array Kopyalamak -> slice, […E56]
 let copyItems = items
-console.log(items)
+console.log(items)  // [' lorem', Array(3), 5, Array(3), 'ipsum']
 copyItems.pop() // son ogeyi cikarttik
-console.log("copyItems", copyItems)
-console.log("items", items)
+console.log("copyItems", copyItems) //[' lorem', Array(3), 5, Array(3)]
+console.log("items:", items)
 
 console.log("kopyalandiktan sonraki hali:")
 copyItems = items.slice() // kopyalama yapti
 copyItems.pop() // son ogeyi cikarttik
 console. log("copyItems", copyItems)
-console.log("items", items)
-let es6Items = [...items] // es6 ve sonrasinda gelen kopyalama islemi
-console.log(es6Items)
+console.log("items", items) // (3) [' lorem', Array(3), 5]
+let es6Items = [...items] // es6 ve sonrasinda gelen kopyalama islemi (4) [' lorem', Array(3), 5, Array(3)]
+console.log(es6Items) // (4) [' lorem', Array(3), 5, Array(3)]
+
+// Iki Array Bilgisini Birlestirmek -> […ES6, ES6]
+let allUsers = [...femaleUsers, ...maleUsers] // es6 birden fazla array yapisini birlestirmek
+console. log(allUsers)
+// Array icerisindeki bilgiyi String'e cevirmek -> toString, join
+console. log(allUsers.toString())
+console. log(allUsers.join(" --- "))
+
+//-Istedigimiz Index Bilgisine Oge Eklemek -> splice(index, 0, value)
+allUsers.splice(allUsers.length -1, 0, "Melissa")
+allUsers.splice(Math.floor( allusers.length /_2 ), 8, "Lorem")
+console. log(allUsers)
