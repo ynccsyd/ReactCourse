5b2cbe9c9311b80d0f3f31dba9d86268c7ab52e1
ynccsyd <muhibbi_10_@hotmail.com>
2023-03-17 15:34:48 +0300

    Added All

--- Javascript/22Array/array1.js
+++ Javascript/22Array/array1.js
@@ -2,19 +2,30 @@
 let domain = "kodluyoruz"
 let isActive = false
 let items = [15, 25, 35, isActive, domain]
-console.log (items)
+console.log(items)
 
- let emptyArray= [] // bos liste
+let emptyArray = [] // bos liste
 // Array içerisindeki eleman/öge sayısını öğrenmek
-console. login
-// items. length // array icimdeki oge sayisi
-// document.queryselector'#into').innerhtml-= items. length
+console.log(
+    items.length    // array icimdeki oge sayisi
+)
+
+// document.queryselector('#into').innerhtml= items.length
+
 // Array icindeki ilk elemanin cagirilmasi
-// console. log( items (0) )
+console.log(items[0])
 // Array icindeki son elemanin cagirilmasi
-// console.log( items[items.length-1] )
+console.log(items[items.length - 1])
 // değişken içindeki bilginin Array olup olmadığının kontrol edilmesi
-// console. log(
-// typeof(items) // object olarak çıktı veriyor
-// console. login
-// Array. isArray (items)
+console.log(
+    typeof (items) // object olarak çıktı veriyor
+)
+console.log(
+    Array.isArray(items)
+)
+
+// hangileri isArray => True verir?
+
+console.log("[]:", Array.isArray([]))
+console.log("1:", Array.isArray(1))
+console.log("true:", Array.isArray(true))
