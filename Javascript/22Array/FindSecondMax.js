aabc5e093d883988d4b5f47ffec737b0914bfe8a
ynccsyd <muhibbi_10_@hotmail.com>
2023-03-17 23:07:45 +0300

    Added reduce

--- Javascript/22Array/FindSecondMax.js
+++ Javascript/22Array/FindSecondMax.js
@@ -21,4 +21,32 @@ const beskati=buyukon.map(function(sayi){
 
 
  // Uygun dizi metotlarını kullanarak, yukarıdaki dizi için aşağıdaki şartları sağlayan myFunction fonksiyonunu yazın.
-// Elemanların arasında 5'ten büyük olan olan bir eleman varsa konsola "Beşten büyük bir eleman mevcut." yoksa "5'ten büyük eleman mevcut değil." yazdır.
+// Elemanların arasında 5'ten büyük olan olan bir eleman varsa konsola
+// "Beşten büyük bir eleman mevcut." yoksa "5'ten büyük eleman mevcut değil." yazdır.
+let dizi1 = [3,6,9,14,16];
+
+// function myFunction (dizi1){
+//     // Kodunuzu buraya yazın.
+//     if (dizi1.some>5){
+//         console.log("Beşten büyük bir eleman mevcut.")
+//     }else{
+//        console.log("5'ten büyük eleman mevcut değil.") 
+//     }
+
+//     }
+    
+// myFunction(dizi1);
+const buyukbes=dizi1.some(function(sayim){
+    return sayim>5
+})
+console.log("Beşten büyük bir eleman mevcut.",buyukbes)
+
+
+// Uygun dizi metodunu kullanarak yukarıda verilen dizinin elemanlarının çarpımının sonucunu 
+//(24) veren bir fonksiyon yazınız.:)
+let dizi2 = [2,3,4];
+function dizicarpim(carpim, item){
+    return carpim*item
+}
+const carpimlar=dizi2.reduce(dizicarpim, 1)
+console.log(carpimlar)
