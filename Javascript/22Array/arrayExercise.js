3c86b8afca972f518248132e0883d6585c0842ff
ynccsyd <muhibbi_10_@hotmail.com>
2023-03-17 19:21:38 +0300

    Added unshift

--- Javascript/22Array/arrayExercise.js
+++ Javascript/22Array/arrayExercise.js
@@ -1,16 +1,17 @@
 let items = [1, 2, 3, 4, 5]
 // Array icinde Array:
-let femaleUsers = ["Ayse", "Hulya", "Herve"]
+let femaleUsers = ["Ayse", "Hulya", "Merve"]
 let maleUsers = ["Ahmet", "Hasan", "Mehmet"]
 items.unshift(femaleUsers)
 items.push(maleUsers)
-console.log(items)
-console. log(items.length)
-console.log(items [0]. length) // Array icindeki istedigimiz Array ' in length bilgisini aldik
-console.log(items[0] [0]) // Ayse bilgisine elastic :)
+console.log(items) // [Array(3), 1, 2, 3, 4, 5, Array(3)]
+console. log(items.length) // 7
+console.log(items [0]. length) //(3) Array icindeki istedigimiz Array ' in length bilgisini aldik
+console.log(items[0] [0]) // Ayse :)
 
 // Array icerisinden oge ayirmak => splice(pos, item?)
-let newItems = items.splice(1, 4) // 1 den başla 4 item al
-console.log("newItems: ", newItems)
-console.log("items: ", items)
+let newItems = items.splice(1, 4) // 1 den başla 4 item al 
+console.log("newItems: ", newItems) // (4) [1, 2, 3, 4]
+console.log("items: ", items) // (3) [Array(3), 5, Array(3)]
+
 // Array icerisindeki ogenin index bilgisini bulmak -> indexOf('value')
