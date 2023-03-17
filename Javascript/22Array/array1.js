2080d9229786ea137f4ef57e440216c41f1f34a8
ynccsyd <muhibbi_10_@hotmail.com>
2023-03-17 15:22:46 +0300

    Added array1.js

---
+++ Javascript/22Array/array1.js
@@ -0,0 +1,20 @@
+// Array olusturmak
+let domain = "kodluyoruz"
+let isActive = false
+let items = [15, 25, 35, isActive, domain]
+console.log (items)
+
+ let emptyArray= [] // bos liste
+// Array içerisindeki eleman/öge sayısını öğrenmek
+// console. login
+// items. length // array icimdeki oge sayisi
+// document.queryselector'#into').innerhtml-= items. length
+// Array icindeki ilk elemanin cagirilmasi
+// console. log( items (0) )
+// Array icindeki son elemanin cagirilmasi
+// console.log( items[items.length-1] )
+// değişken içindeki bilginin Array olup olmadığının kontrol edilmesi
+// console. log(
+// typeof(items) // object olarak çıktı veriyor
+// console. login
+// Array. isArray (items)
