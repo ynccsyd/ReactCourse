3d32237ccd62746c6b1bbcbe87e2d197384c6abd
ynccsyd <muhibbi_10_@hotmail.com>
2023-03-17 21:37:26 +0300

    Added js

--- Javascript/22Array/arrayExercise.js
+++ Javascript/22Array/arrayExercise.js
@@ -84,9 +84,119 @@ malzemeler.forEach(function(malzeme, malzemeIndeksi) {
 //Map metodu da forEach gibi kendisine verilen fonksiyonu dizinin her elemanı için uygular 
 //fakat forEach'ten farklı olarak sonucu yeni bir dizide tutar. Orijinal dizi olduğu gibi kalır.
 // Bir dizideki sayıların 5 katından oluşan başka bir dizi oluşturalım:
-const sayilar = [1,2,3];
-let sayilarin5kati = sayilar.map(function(sayi) {
+const sayilarim = [1,2,3];
+let sayilarin5kati = sayilarim.map(function(sayi) {
     return sayi*5;
 });
 console.log(sayilarin5kati);
-console.log(sayilar);
+console.log(sayilarim);
+
+/* .some() */
+const sayilar2 = [4,5,6];
+// sonucu görebilmek çıktıyı sonuc adlı değişkende tutalım:
+const sonuc1 = sayilar2.some(function(sayi) {
+    return sayi > 5 ;
+});
+console.log(sonuc1);
+// Dizi içerisinde 5'ten büyük bir eleman bulunduğu için çıktıda "true" görmeyi bekleriz.
+const sonuc2 = sayilar2.some(function(sayi) {
+    return sayi <3;
+});
+
+console.log(sonuc2);
+// Dizi içerisinde 3'ten küçük herhangi bir sayı olmadığı için çıktıda "false" görmeyi bekleriz. 
+
+// .filter()
+// filter metodu bir dizi içerisindeki belirli bir koşulu sağlayan elemanlar ile yeni bir dizi oluşturmamıza dolayısıyla dizi elemanlarını filtrelememize yarıyor.
+// Yeni oluşacak diziyi bir değişkende saklıyoruz.
+// Orijinal dizi bozulmuyor.
+
+
+const sayilar1 = [1,2,3,4,5];
+
+const filtrelenmisSayilar = sayilar1.filter(function(sayi) {
+    return sayi >3;
+});
+
+console.log(filtrelenmisSayilar);
+// Orijinal diziyi 3'ten büyük olan sayılar için filtrelediğimizde yeni oluşacak dizi [4,5] olacaktır.
+
+console.log(sayilar1);
+// Orijinal dizi bozulmayacağından çıktıda [1,2,3,4,5] olarak görürüz.
+
+
+// .find()
+// Bu metot belirtilen koşula uyum sağlayan dizi elemanını bulmamızı sağlar.
+// Diğer metotların aksine find metodu elemanın kendisini döner.
+// Koşulu sağlayan birden fazla eleman varsa, bulduğu ilk elemanı döner.
+// Koşulu sağlayan bir eleman bulamazsa undefined döner.
+// Örneğin:
+
+const sayilar = [4,5,6,7];
+
+const bulunacakEleman1 = sayilar.find(function(sayi) {
+    return sayi === 5;
+});
+console.log(bulunacakEleman1);
+// Dizi içerisinde 5 eleman olarak bulunduğu için çıktıda elemanın kendisini yani 5 görmeyi bekleriz.
+
+
+const bulunacakEleman2 = sayilar.find(function(sayi) {
+    return sayi > 5;
+});
+console.log(bulunacakEleman2);
+// Dizi içerisinde 5'ten büyük birden fazla eleman olduğu için koşula uyan ilk elemanı yani 6'yı görmeyi bekleriz.
+
+
+const bulunacakEleman3 = sayilar.find(function(sayi) {
+    return sayi < 3;
+});
+console.log(bulunacakEleman3);
+// Dizi içerisinde 3'ten küçük bir eleman olmadığı için çıktıda undefined görmeyi bekleriz.
+
+// .sort()
+// Bu metot, dizi içerisindeki elemanları sıralamaya yarar.
+// Orijinal dizi sıralanmış şekilde döner. (Yani dizi mutate eder.)
+// Eğer parametre olarak bir fonksiyon verilmezse dizi elemanları string'e çevrilir ve UTF-16 değerlerine göre sıralanır.
+// Artan veya azalan olarak sıralamak için farklı bir yol izlenmelidir.
+// Örneğin:
+// Varsayılan haliyle (parametre vermeden) sıralamak yapmak istediğimizde:
+const sayilar3 = [3,5,2,10,4];
+
+sayilar.sort();
+console.log(sayilar3);
+// Çıktı olarak [10, 2, 3, 4, 5] alırız. 
+//(Sayıların string halleri UTF-16 değerlerine göre sıralandıkları için)
+const sayilar4 = [3,5,2,10,4];
+
+sayilar.sort(function(a,b) {
+    return b-a;
+});
+console.log(sayilar4);
+// Çıktı olarak [10, 5, 4, 3, 2] alırız.
+
+
+// .reduce()
+// Bu metot dizimizi yalnızca bir değere indirger.
+// Parantez içerisine parametre olarak bir fonksiyon ve accumulator'ün (toplayıcının) başlangıç değeri girilir.
+// Bu metot orijinal diziyi değiştirmez. (Mutate etmez.)
+// Tam olarak ne yaptığını anlamak için örnekleri inceleyelim.
+// Örnekler:
+
+const sayilarr= [10,20,30];
+
+// Dizi içindeki sayıları toplayarak indirgeyecek bir fonksiyon yazalım:
+function indirgeyici (akumulator, sayi) {
+    return akumulator + sayi;
+}
+
+// Bu fonksiyonu ve toplamaya 0'dan başlayacağımızı belirten 0 sayısını metodumuza parametre olarak girelim ve sonucu bir değişkende tutalım:
+const sonucbir = sayilarr.reduce(indirgeyici,0);
+
+console.log(sonucbir);
+//0 + 10 + 20 + 30 = 60 olacağından çıktı olarak 60 bekleriz.
+// Eğer akümülatorümüzü 0 yerine 5'den başlatsaydık çıkabilecek sonucu görelim:
+const sonuciki = sayilar.reduce(indirgeyici, 5);
+
+console.log(sonuciki);
+// 5 + 10 + 20 + 30 = 65 olacağından çıktı olarak 65 bekleriz.
