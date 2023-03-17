c12c1fe5837f4955bf79a80438def795da594da2
ynccsyd <muhibbi_10_@hotmail.com>
2023-03-17 20:10:19 +0300

    Added arrayPop

--- Javascript/22Array/arrayExercise.js
+++ Javascript/22Array/arrayExercise.js
@@ -15,3 +15,22 @@ console.log("newItems: ", newItems) // (4) [1, 2, 3, 4]
 console.log("items: ", items) // (3) [Array(3), 5, Array(3)]
 
 // Array icerisindeki ogenin index bilgisini bulmak -> indexOf('value')
+
+items. unshift(" lorem")
+items. push("ipsum")
+console.log( items.index0f("ipsum"))
+
+//Array Kopyalamak -> slice, […E56]
+let copyItems = items
+console.log(items)
+copyItems.pop() // son ogeyi cikarttik
+console.log("copyItems", copyItems)
+console.log("items", items)
+
+console.log("kopyalandiktan sonraki hali:")
+copyItems = items.slice() // kopyalama yapti
+copyItems.pop() // son ogeyi cikarttik
+console. log("copyItems", copyItems)
+console.log("items", items)
+let es6Items = [...items] // es6 ve sonrasinda gelen kopyalama islemi
+console.log(es6Items)
