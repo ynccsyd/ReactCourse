3ab743176fab772f43b6905e8eee2936027f8f20
ynccsyd <muhibbi_10_@hotmail.com>
2023-03-18 13:36:25 +0300

    Added list

---
+++ Javascript/23ForDöngüsü/for_1.js
@@ -0,0 +1,15 @@
+// For Dongusu
+// https://developer.mozilla.org/tr/docs/Web/JavaScript/Guide/
+// Loops_and_iteration
+
+// for ([başlangıçAtaması]; [koşulİfadesi]; [arttırımİfadesi]) {
+// // yapilacak islam
+//
+let users = ["Lorem", "Ipsum", "Dolor",]
+// for (let index = 0; index < 10; index++) {
+// console.log(index)
+// }
+let index =0
+for (; index < 10; index++) {
+    console.log(index)
+}
