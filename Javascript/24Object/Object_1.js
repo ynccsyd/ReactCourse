3a1c0e193ab2da8b550f124598c832dde3a403b6
ynccsyd <muhibbi_10_@hotmail.com>
2023-03-23 19:50:49 +0300

    Added object

---
+++ Javascript/24Object/Object_1.js
@@ -0,0 +1,6 @@
+//Objeler, içerisinde birden fazla değeri depolayan yapılardır.
+// Property olarak bilinen bu değerler primitive(String,number, boolean 
+//vb.) veya başka objeler olabilirler ve her bir değer string veya sembol ile isimlendirilebilir.
+
+//Objeler değiştirilebilen veri tiplerindendir. İçerdiği propertyler, objenin 
+//kendisi const keyword ile atanmış olsa bile değiştirilebilirler.
