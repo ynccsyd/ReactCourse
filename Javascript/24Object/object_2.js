263cd505c950ac115088ac1f0b49bfc960c9c730
ynccsyd <muhibbi_10_@hotmail.com>
2023-03-24 19:00:11 +0300

    Added Object_2.js

---
+++ Javascript/24Object/object_2.js
@@ -0,0 +1,16 @@
+// [key1:1, key1:2, key3:3,
+let laptop1 =
+brand: "Apple",
+model: "MacBook Pro",
+2kg": 2
+model Name: "Hackbook Pro",
+model-name: "Hackbook Pro",
+model_name: "Hackbook Pro"
+console. log(laptop1)
+console.log(laptop 1.brand, laptop 1["brand"])
+console. log(laptop 1.model, laptop 1["model"])
+console. log (laptop 1 ["2kg"1)
+// Dogru Anahtar Bilgisi Olusturmak
+// // Anahtar bilgisine yenideger eklemek
+// Anahtar Bilgilerine Ulasmak (keys) -> Object keys(item)
+/ Deger Bilgilerine Ulasmak (values) - Object values(item)
