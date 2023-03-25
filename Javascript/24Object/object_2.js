83a03807518e064ffa72ad3b2bdcea85e462a0e7
ynccsyd <muhibbi_10_@hotmail.com>
2023-03-25 11:35:47 +0300

    Added js

--- Javascript/24Object/object_2.js
+++ Javascript/24Object/object_2.js
@@ -22,7 +22,7 @@ laptop1.version = "10.15.7"
 console.log(laptop1)
 
 //Anahtar Bilgilerine Ulasmak (keys) -> Object.keys(item)
-keys = Object, keys(laptop1)
+keys = Object.keys(laptop1)
 console.log(keys)
 console.log(Object.keys(laptop1))
 
@@ -40,3 +40,56 @@ let values = Object.values(laptop1)
 values.forEach(value => {
     console. log("value: ", value)
 })
+
+// Samples
+let person= {};                     // set (oluştur)
+person["likes sea"] = true;         // get (getir)
+console.log(person["likes sea"]);   // true (doğru)
+delete person["likes sea"];         // delete (sil)
+
+//Köşeli parantezli yazım aynı zamanda property name getirmede kullanılabilir:
+let animal= {};           // set
+let key = "flies";        // key atama
+animal[key] = true;       // value atama
+
+//
+let person1 = {
+    name: "Jack",
+    age: 20,
+  };
+  let key1 = prompt("Kişinin hangi özelliğini öğrenmek isterdiniz?", "name");
+  
+  // değişken ile erişim
+  alert(person1[key1]);      // Jack (prompt’a “name” yazarsak erişeceğimiz değer)
+
+
+//Computed properties, objedeki hangi property'nin güncelleneceğini dinamik olarak seçmemize olanak tanır.
+function objectify (key, value) {
+    return {
+ [key]: value
+   }
+ }
+ objectify("name", "Anna");   //  {name: "Anna"} atanmış yeni değer
+
+
+ /* KEY VALUE */
+ let state = {
+	users:[
+		{name: "Brock", age: 25, favoriteColor: "red"},
+		{name: "Jessie", age: 17, favoriteColor: "yellow"},
+		{name: "James", age: 41, favoriteColor: "blue"},
+		{name: "Winnie", age: 18, favoriteColor: "purple"}
+	],
+	settings:{
+		version: "1.0.5",
+        DNS: "105.xx.xx.xx",
+        website: "https://www.example.com/"
+	},
+	banList: ["Ash", "Angelica", "Tom", "Jerry"]
+}
+//"banList" propertysinde 3. elemana ulaşmak istersek
+alert(state.banList[2])
+//"settings"lerdeki "website" değerine ulaşmak istersek 
+alert(state.settings.website)
+//"user"larda James'in en sevdiği rengi öğrenmek istersek, o veriye şu şekilde ulaşabiliriz.
+alert(state.users[2].favoriteColor) //blue
