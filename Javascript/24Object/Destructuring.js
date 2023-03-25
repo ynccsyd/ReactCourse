a6f6948eb2015c7bd0e996b96f90c7a3025c6fcb
ynccsyd <muhibbi_10_@hotmail.com>
2023-03-25 13:18:43 +0300

    Added Destructuring

--- Javascript/24Object/Destructuring.js
+++ Javascript/24Object/Destructuring.js
@@ -1,3 +1,16 @@
 /*
  Destructuring bir obje veya bir array içinden her bir 
  elemanın alınıp bir değişken içine kaydedilmesi. */
+
+//  Destructuring bir fonksiyonun bir objeyi parametre olarak alması sırasında çok kullanışlıdır. 
+//  Yani fonksiyona parametre girerken de destructuring yapabiliriz;
+const cikarma = ( { sayi1, sayi2 } ) => { 
+    // Bu satira dikkat
+    ﻿return sayi1 - sayi2;
+    }
+    const sayilar = {
+     sayi2: 3,
+     sayi1: 5
+    }
+    cikarma(sayilar); 
+    // sonuc 2
