56dd6b3aeb00269f50dcf65ae3cd29c9e1e3db1e
ynccsyd <muhibbi_10_@hotmail.com>
2023-03-25 11:39:48 +0300

    Added Object_OZET

---
+++ Javascript/24Object/Object_JSON.js
@@ -0,0 +1,7 @@
+/*Projenizde JSON dosyalarıyla çalışabilmeniz için önce onu dosyada 
+import etmeniz ve obje şekline çevirmeniz gerekmektedir.*/
+
+
+const veri = require("./dosyanizin/dizini/veri.json") // Bu işlemde require fonksiyonu ile dosyanızı import etmektesiniz
+
+// Bu işlemden sonra veri bileşenini obje şeklinde kullanabilirsiniz.
