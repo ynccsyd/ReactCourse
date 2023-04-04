27d37183d9b5952db6c68d29235d4b673a846eda
ynccsyd <muhibbi_10_@hotmail.com>
2023-04-04 17:23:34 +0300

    Added Footer

--- react-todo-app/src/components/footer.js
+++ react-todo-app/src/components/footer.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className='info'>
-            <p>Click to edit a todo</p>
-            <p>Created by <a href="https://d12n.me/">Dmitry Sharabin</a></p>
-            <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
-        </footer>
+        <div>
+            <footer class="info">
+                <p>Click to edit a todo</p>
+                <p>Created by <a href="https://d12n.me/">Dmitry Sharabin</a></p>
+                <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+            </footer>
+        </div>
     )
 }
-
 export default Footer
--- react-todo-app/src/components/list.js
+++ react-todo-app/src/components/list.js
@@ -1,9 +1,64 @@
 import React from 'react'
 
 const List = () => {
-  return (
-    <div>List</div>
-  )
+    return (
+        <div>
+            <section class="main">
+                <input class="toggle-all" type="checkbox" />
+                <label for="toggle-all">
+                    Mark all as complete
+                </label>
+
+                <ul class="todo-list">
+                    <li class="completed">
+                        <div class="view">
+                            <input class="toggle" type="checkbox" />
+                            <label>Learn JavaScript</label>
+                            <button class="destroy"></button>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="view">
+                            <input class="toggle" type="checkbox" />
+                            <label>Learn React</label>
+                            <button class="destroy"></button>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="view">
+                            <input class="toggle" type="checkbox" />
+                            <label>Have a life!</label>
+                            <button class="destroy"></button>
+                        </div>
+                    </li>
+                </ul>
+            </section>
+
+            <footer class="footer">
+                <span class="todo-count">
+                    <strong>2</strong>
+                    items left
+                </span>
+
+                <ul class="filters">
+                    <li>
+                        <a href="#/" class="selected">All</a>
+                    </li>
+                    <li>
+                        <a href="#/">Active</a>
+                    </li>
+                    <li>
+                        <a href="#/">Completed</a>
+                    </li>
+                </ul>
+
+                <button class="clear-completed">
+                    Clear completed
+                </button>
+            </footer>
+
+        </div>
+    )
 }
 
 export default List
