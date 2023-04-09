6e5152e38a829e16f1632662b44ebcbb35b1492a
ynccsyd <muhibbi_10_@hotmail.com>
2023-04-09 22:29:07 +0300

    Added Catch isLoading

--- react-native-fetch/src/component/users.js
+++ react-native-fetch/src/component/users.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 
 const Users = () => {
-  return (
-    <div>Users</div>
-  )
+    const[users, setUsers]=useState([])
+   const [isLoading, setisLoading] = useState(true); //extention usf
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((res) => res.json())
+            .then((data) => setUsers(data))
+            .catch((e)=> console.log(e))
+            .finally(() => {
+              setisLoading(false)
+            } )
+         
+    }, []);
+
+    return (
+        <div>
+            <h1>Users</h1>
+            {isLoading && <div>Loading...</div> }
+            {
+                users.map((user)=>(
+                    <div key={user.id}>{user.name} </div>
+                ) ) 
+            }
+        </div>
+    )
 }
 
 export default Users
