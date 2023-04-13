46c669a922395620ee85c1efa152dd5c4675d105
ynccsyd <muhibbi_10_@hotmail.com>
2023-04-13 20:31:06 +0300

    Added ObjectSamples

--- Javascript/24Object/Destructuring.js
+++ Javascript/24Object/Destructuring.js
@@ -38,3 +38,37 @@ const toplama = ({ sayi1, sayi2, ...args }) => {
 let person={name:"Selin",city:"Ankara",favoriteColor:"aqua blue"}; 
 let {name:foo}=person;
 console.log(foo) // Selin
+
+var house2 = {};
+house2["rooms"] = 4;
+house2['color']= "pink";
+house2["priceUSD"] = 12345;
+console.log(house2); // {rooms: 4, color: 'pink', priceUSD: 12345}
+
+/****/
+var car = {};
+car.color = "red";
+car["color"] = "green";
+car["speed"] = 200;
+car.speed = 100;
+console.log(car); // {color: "green", speed: 100}
+
+car["number of doors"] = 4;
+console.log(car); // {color: 'green', speed: 100, number of doors: 5}
+
+car["2022"] = 1901;
+console.log(car); // {2022: 1901, color: 'green', speed: 100, number of doors: 5}
+
+/*** */
+var arrOfKeys = ['speed', 'altitude', 'color'];
+var drone = {
+    speed: 100,
+    altitude: 200,
+    color: "red"
+}
+for (var i = 0; i < arrOfKeys.length; i++) {
+    console.log(drone[arrOfKeys[i]])
+} 
+// 100
+// 200
+// red
