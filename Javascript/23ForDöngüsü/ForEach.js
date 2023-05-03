d69eadc3ff90b5540e23b8cc161666f90172b17e
ynccsyd <muhibbi_10_@hotmail.com>
2023-05-03 13:58:30 +0300

    Added ForEach

--- Javascript/23ForDöngüsü/ForEach.js
+++ Javascript/23ForDöngüsü/ForEach.js
@@ -39,3 +39,19 @@ numbers.forEach(function(numbers){
 console.log(newArray);
 
 // output = [12, 33, 27]
+
+/** */
+const fruits = ['kiwi','mango','apple','pear'];
+function appendIndex(fruit, index) {
+    console.log(`${index}. ${fruit}`)
+}
+fruits.forEach(appendIndex);
+// 0. kiwi
+// 1. mango
+// 2. apple
+// 3. pear
+
+const veggies = ['onion', 'garlic', 'potato'];
+veggies.forEach( function(veggie, index) {
+    console.log(`${index}. ${fruit}`);
+});
