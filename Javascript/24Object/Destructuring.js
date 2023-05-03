1f3336fb31df60f4442638f76b9141900a36531d
ynccsyd <muhibbi_10_@hotmail.com>
2023-05-03 12:31:34 +0300

    Added ObjectLoop

--- Javascript/24Object/Destructuring.js
+++ Javascript/24Object/Destructuring.js
@@ -72,3 +72,68 @@ for (var i = 0; i < arrOfKeys.length; i++) {
 // 100
 // 200
 // red
+ // Nesneler yinelenebilir (iterable) değildir elemanlarını dizi olarak yazmak için;
+ const car = {
+    speed: 100,
+    color: "blue"
+}
+
+for(prop of car) {
+    console.log(prop)
+} // Uncaught TypeError: car is not iterable
+
+const colors = ['red','orange','yellow']
+for (var color of colors) {
+    console.log(color);
+} //Output;
+// red
+// orange
+// yellow
+
+/*Object.keys(), Object.values(), and Object.entries().*/
+const car2 = {
+    speed: 200,
+    color: "red"
+}
+console.log(Object.keys(car2)); // ['speed','color']
+
+const car3 = {
+    speed: 300,
+    color: "yellow"
+}
+console.log(Object.values(car3)); // [300, 'yellow']
+
+const car4 = {
+    speed: 400,
+    color: 'magenta'
+}
+console.log(Object.entries(car4)); // [ ['speed', 400], ['color', 'magenta'] ]
+
+/**Make a loop */
+var clothingItem = {
+    price: 50,
+    color: 'beige',
+    material: 'cotton',
+    season: 'autumn'
+}
+
+for( key of Object.keys(clothingItem) ) {
+    console.log(keys, ":", clothingItem[key])
+}
+//how to access an object's member using the brackets notation. 
+function testBracketsDynamicAccess() {
+    var dynamicKey;
+    if(Math.random() > 0.5) {
+      dynamicKey = "speed";
+     }else{
+       dynamicKey = "color";
+     }
+  
+      var drone = {
+        speed: 15,
+        color: "orange"
+      }
+  
+      console.log(drone[dynamicKey]);
+  }
+  testBracketsDynamicAccess();
