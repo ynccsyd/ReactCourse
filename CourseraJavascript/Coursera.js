6150f1540e4db1c1923b480e9b28db4e3a5e94d1
ynccsyd <muhibbi_10_@hotmail.com>
2023-05-04 13:04:42 +0300

    Added Codes

--- CourseraJavascript/Coursera.js
+++ CourseraJavascript/Coursera.js
@@ -5,3 +5,32 @@ if (typeof(answer) === 'string') {
     document.body.innerText = '';
     document.body.appendChild(h1);
 }
+//
+// var h1 = document.createElement('h1')
+// h1.innerText = "Type into the input to make this text change"
+
+// var input = document.createElement('input')
+// input.setAttribute('type', 'text')
+
+// document.body.innerText = '';
+// document.body.appendChild(h1);
+// document.body.appendChild(input);
+
+// input.addEventListener('change', function() {
+//     console.log(input.value)
+// })
+
+//
+var h1 = document.createElement('h1')
+h1.innerText = "Type into the input to make this text change"
+
+var input = document.createElement('input')
+input.setAttribute('type', 'text')
+
+document.body.innerText = '';
+document.body.appendChild(h1);
+document.body.appendChild(input);
+
+input.addEventListener('change', function() {
+    h1.innerText = input.value
+})
