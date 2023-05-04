2c438aa1f58031a88137bcde6a37e87782ec6b33
ynccsyd <muhibbi_10_@hotmail.com>
2023-05-04 12:54:47 +0300

    Added Array sample

--- Javascript/22Array/array2.js
+++ Javascript/22Array/array2.js
@@ -71,3 +71,9 @@ const car2 = {...car1}
 car1.speed = 201
 console.log(car1.speed, car2.speed)
 //Output: 201, 200
+
+const fruits1 = ['apples', 'pears']
+const fruits2 = [...fruits]
+fruits1.pop()
+console.log(fruits1, "not", fruits2)
+//['apples'] 'not' ['apples','pears']
