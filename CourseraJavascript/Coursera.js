b31fb1c464d4996a4368b25a88e5b84de61522bb
ynccsyd <muhibbi_10_@hotmail.com>
2023-05-04 12:56:28 +0300

    Added Coursera JS

---
+++ CourseraJavascript/Coursera.js
@@ -0,0 +1,7 @@
+let answer = prompt('What is your name?');
+if (typeof(answer) === 'string') {
+    var h1 = document.createElement('h1')
+    h1.innerText = answer;
+    document.body.innerText = '';
+    document.body.appendChild(h1);
+}
