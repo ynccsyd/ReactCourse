a2e618aa2b104f1828299ecd4c8b5aee0b61ae90
ynccsyd <muhibbi_10_@hotmail.com>
2023-05-05 17:26:22 +0300

    Added All

--- CourseraJavascript/Coursera.js
+++ CourseraJavascript/Coursera.js
@@ -1,11 +1,26 @@
-let answer = prompt('What is your name?');
-if (typeof(answer) === 'string') {
-    var h1 = document.createElement('h1')
-    h1.innerText = answer;
-    document.body.innerText = '';
-    document.body.appendChild(h1);
-}
-//
+// let answer = prompt('What is your name?');
+// if (typeof(answer) === 'string') {
+//     var h1 = document.createElement('h1')
+//     h1.innerText = answer;
+//     document.body.innerText = '';
+//     document.body.appendChild(h1);
+// }
+// //
+// // var h1 = document.createElement('h1')
+// // h1.innerText = "Type into the input to make this text change"
+
+// // var input = document.createElement('input')
+// // input.setAttribute('type', 'text')
+
+// // document.body.innerText = '';
+// // document.body.appendChild(h1);
+// // document.body.appendChild(input);
+
+// // input.addEventListener('change', function() {
+// //     console.log(input.value)
+// // })
+
+// //
 // var h1 = document.createElement('h1')
 // h1.innerText = "Type into the input to make this text change"
 
@@ -17,20 +32,33 @@ if (typeof(answer) === 'string') {
 // document.body.appendChild(input);
 
 // input.addEventListener('change', function() {
-//     console.log(input.value)
+//     h1.innerText = input.value
 // })
-
 //
-var h1 = document.createElement('h1')
-h1.innerText = "Type into the input to make this text change"
+var h1= document.querySelector('h1')
+var arr=[
+    'Example Domain',
+    'First Click',
+    'Second Click',
+    'Third Click'
+]
+console.log(arr)
+function  handleClicks(){
+ switch(h1.innerText){
+    case arr[0]:
+        h1.innerText = arr[1];
+        break;
+    case  arr[1]:
+        h1.innerText = arr[2];
+        break;
+    case arr[2]:
+        h1.innerText = arr[3];
+        break; 
+    default:
+        h1.innerText = arr[0]
+ }
+  
+}
 
-var input = document.createElement('input')
-input.setAttribute('type', 'text')
+h1.addEventListener('click',handleClicks);
 
-document.body.innerText = '';
-document.body.appendChild(h1);
-document.body.appendChild(input);
-
-input.addEventListener('change', function() {
-    h1.innerText = input.value
-})
