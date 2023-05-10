23c448509f9bddb1ad68295c4850d44420bd665e
ynccsyd <muhibbi_10_@hotmail.com>
2023-05-10 13:13:04 +0300

    added Components

--- ReactCoursera/state-managment/src/App.js
+++ ReactCoursera/state-managment/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Fruits from "./Fruits";
+import FruitsCounter from "./FruitsCounter";
 
 function App() {
+  const [fruits] = React.useState([
+        {fruitName: 'apple', id: 1},
+        {fruitName: 'apple', id: 2},
+        {fruitName: 'plum', id: 3},
+    ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Where should the state go?</h1>
+      <Fruits fruits={ fruits}/>
+      <FruitsCounter  fruits={ fruits} />
     </div>
   );
 }
