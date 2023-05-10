dd04a535f1b979ed4a8c0ff4650f9c51c6a43b8d
ynccsyd <muhibbi_10_@hotmail.com>
2023-05-10 12:27:31 +0300

    Added Prop Drilling

--- ReactCoursera/react-hooks/src/App.js
+++ ReactCoursera/react-hooks/src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import RegisterForm from './RegisterForm';
 import TextInputWithFocusButton from './TextInputWithFocusButton';
-
+import AppProp from './PropDrilling';
 function App() {
   return (
     <div className="App">
       <RegisterForm />
       <TextInputWithFocusButton />
+      <AppProp/>
     </div>
   );
 }
 
 export default App;
+
+
