f12393caf3503ae2126bf76db58689daddb4f322
ynccsyd <muhibbi_10_@hotmail.com>
2023-05-21 13:36:21 +0300

    Added App.js

--- ReactCoursera/calculator_app/src/App.js
+++ ReactCoursera/calculator_app/src/App.js
@@ -1,9 +1,6 @@
-import {
-  useState,
-  useRef
-} from "react"; 
-import "./App.css";
-
+import React, { useState, useRef } from "react"; 
+import "./App.css"; 
+ 
 function App() { 
   const inputRef = useRef(null); 
   const resultRef = useRef(null); 
@@ -11,68 +8,65 @@ function App() {
  
   function plus(e) { 
     e.preventDefault(); 
+    // const inputVal = inputRef.current.value; 
+    // const newResult = result + Number(inputVal); 
+    // setResult(newResult); 
     setResult((result) => result + Number(inputRef.current.value)); 
-  }; 
+  } 
  
   function minus(e) { 
-  	// Add the code for the minus function 
     e.preventDefault(); 
-    setResult((result) => result - Number(inputRef.current.value)); 
-  }; 
+    const inputVal = inputRef.current.value; 
+    const newResult = result - Number(inputVal); 
+    setResult(newResult); 
+  } 
  
   function times(e) { 
-    // Add the code for the plus function 
     e.preventDefault(); 
-    setResult((result) => result * Number(inputRef.current.value)); 
-  };  
+    const inputVal = inputRef.current.value; 
+    const newResult = result * Number(inputVal); 
+    setResult(newResult); 
+  } 
  
   function divide(e) { 
-    // Add the code for the divide function 
     e.preventDefault(); 
-    setResult((result) => result / Number(inputRef.current.value)); 
-  }; 
+    const inputVal = inputRef.current.value; 
+    const newResult = result / Number(inputVal); 
+    setResult(newResult); 
+  } 
  
   function resetInput(e) { 
-    // Add the code for the resetInput function 
-    e.preventDefault();
-    inputRef.current.value = 0;
-  }; 
+    e.preventDefault(); 
+    inputRef.current.value = 0; 
+  } 
  
   function resetResult(e) { 
-  	// Add the code for the resetResult function 
-    e.preventDefault();
-    setResult((preVal) => preVal * 0);
-  };
+    e.preventDefault(); 
+    setResult(0); 
+  } 
+ 
   return ( 
     <div className="App"> 
       <div> 
         <h1>Simplest Working Calculator</h1> 
       </div> 
       <form> 
-        <p ref={resultRef}> 
-          {/* add the value of the current total */} 
-        </p> 
-        <input
+        <p ref={resultRef}>{result}</p> 
+        <input 
           pattern="[0-9]" 
           ref={inputRef} 
           type="number" 
           placeholder="Type a number" 
         /> 
-       <button onClick={plus}>add</button>
-        {/* Add the subtract button */}
-        <button onClick={minus}>subtract</button>
-        {/* Add the multiply button */}
-        <button onClick={times}>multiply</button>
-        {/* Add the divide button */}
-        <button onClick={divide}>divide</button>
-        {/* Add the resetInput button */}
-        <button onClick={resetInput}>reset input</button>
-        {/* Add the resetResult button */}
-        <button onClick={resetResult}>reset result</button>
-      
+        <button onClick={plus}>add</button> 
+        <button onClick={minus}>subtract</button> 
+        <button onClick={times}>multiply</button> 
+        <button onClick={divide}>divide</button> 
+        <button onClick={resetInput}>reset input</button> 
+        <button onClick={resetResult}>reset result</button> 
       </form> 
     </div> 
   ); 
 } 
  
-export default App; 
+export default App;
