ff87a632597b59328665f939252956926fad92ff
ynccsyd <muhibbi_10_@hotmail.com>
2023-05-30 12:48:10 +0300

    Added Form Control

---
+++ ReactCoursera/registration_form/src/FORM.js
@@ -0,0 +1,41 @@
+import './App.css';
+import { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  const handleSubmit= (e) => {
+   
+    e.preventDefault();
+    setName("");  //to clear input
+    console.log("Form submitted!")
+  }
+  return (
+    <div className="App">
+
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <div className="field">
+            <label htmlFor="">Name</label>
+            <input 
+            id='Name'
+            type="text" 
+            name="name" 
+            placeholder='Name' 
+            value={name} 
+            onChange={(e) => 
+              setName(e.target.value)
+
+            } />
+          </div>
+          <button 
+          disabled={!name}  // when enter some letters it will able/active
+          type="submit">
+            Submit
+          </button>
+        </fieldset>
+      </form>
+    </div>
+  );
+}
+
+export default App;
