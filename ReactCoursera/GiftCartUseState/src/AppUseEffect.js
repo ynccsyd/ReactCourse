6f2947f1c7931e1cb8b8aaba29d958a38208f24c
ynccsyd <muhibbi_10_@hotmail.com>
2023-06-18 22:57:40 +0300

    Added UseEffect

---
+++ ReactCoursera/GiftCartUseState/src/AppUseEffect.js
@@ -0,0 +1,20 @@
+import React, {useState} from "react";
+export default function App(){
+  const [toggle, setToggle] = useState(false);
+
+  const clickHandler= () => {
+    setToggle(!toggle)
+  }
+  React.useEffect( () => {
+    document.title=toggle ? "Welcome to Little Lemon" : " Using useEffect hook"
+  }, [toggle])
+  return(
+    <div>
+      <h1>Using the useEffect hook</h1>
+      <button onClick={clickHandler}>
+        Toggle Message
+      </button>
+      {toggle && <h2>Welcome to Little Lemon</h2> }
+    </div>
+  )
+}
