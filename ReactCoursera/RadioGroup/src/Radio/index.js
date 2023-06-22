003231036f3908e8376eceae9a7708c863ed0ea6
ynccsyd <muhibbi_10_@hotmail.com>
2023-06-22 12:22:32 +0300

    Added RadioOptionMAp

--- ReactCoursera/RadioGroup/src/Radio/index.js
+++ ReactCoursera/RadioGroup/src/Radio/index.js
@@ -4,18 +4,33 @@ import "./styles.css";
 export const RadioGroup = ({ onChange, selected, children }) => {
   // Use React.Children.map and React.cloneElement to clone the children
   // and pass the correct props to each RadioOption
-  const RadioOptions = null;
-
-  return <div className="RadioGroup">{RadioOptions}</div>;
-};
+  // The first argument is the children prop, and the second argument is 
+  //a transformation function that returns a new React element. 
+  const RadioOptions = React.Children.map(children, (child) => { 
+    return React.cloneElement(child, { 
+      onChange, 
+      checked: child.props.value === selected, 
+    }); 
+  }); 
+  return <div className="RadioGroup">{RadioOptions}</div>; 
+ };
 
 export const RadioOption = ({ value, checked, onChange, children }) => {
   // Hook up the onChange handler to call the onChange prop passed to RadioGroup
   // Also, make sure to pass the correct checked prop to the input element
   return (
-    <div className="RadioOption">
-      <input id={value} type="radio" name={value} />
-      <label htmlFor={value}>{children}</label>
-    </div>
+    <div className="RadioOption"> 
+     <input 
+       id={value} 
+       type="radio" 
+       name={value} 
+       value={value} 
+       checked={checked} 
+       onChange={(e) => { 
+         onChange(e.target.value); 
+       }} 
+     /> 
+     <label htmlFor={value}>{children}</label> 
+   </div> 
   );
 };
