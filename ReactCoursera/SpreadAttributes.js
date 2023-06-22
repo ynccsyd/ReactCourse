292b45ed9e56ea5d50c34032338b903693d1e8c9
ynccsyd <muhibbi_10_@hotmail.com>
2023-06-22 12:50:34 +0300

    Added Spread Attributes

--- ReactCoursera/SpreadAttributes.js
+++ ReactCoursera/SpreadAttributes.js
@@ -1,12 +1,45 @@
 import "./App.css";
 
-const Button=({type, children, ...buttonProps})=>{
-    const className=type==="primary" ? "PrimaryButton": "SecondaryButton";
-    return(
+const Button = ({ type, children, ...buttonProps }) => {
+    const className = type === "primary" ? "PrimaryButton" : "SecondaryButton";
+    return (
         <button className={`Button ${className}`} {...buttonProps}>
             {children}
         </button>
     )
 };
 
+const LogginButton = ({ type, children, ...buttonProps }) => {
+    return (
+        <Button
+            type="secondary"
+            {...buttonProps}
+            onClick={() => {
+                alert("Logging in!");
+            }}
+        >
+            {children}
+        </Button>
+    )
+};
 
+function App() {
+    return (
+        <div className="App">
+            <header className="Header">Little Lemon Restaurant</header>
+            <Button type="primary" onClick={() => {
+                alert("Signing Up!")
+            }}>
+                Sign Up
+            </Button>
+            <LogginButton
+                type="secondary" onClick={() => {
+                  alert("Signing Up!")
+                }}
+            >
+                Login
+            </LogginButton>
+        </div>
+    )
+}
+export default App;
