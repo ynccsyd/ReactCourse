12c677983b47c7c4f3733ea80eb70135a959a708
ynccsyd <muhibbi_10_@hotmail.com>
2023-06-28 16:42:28 +0300

    Added Card Style

--- ReactCoursera/PortfolioApp/src/components/Card.js
+++ ReactCoursera/PortfolioApp/src/components/Card.js
@@ -7,26 +7,26 @@ const Card = ({ title, description, imageSrc }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
-  
+
   return (
-    <HStack
-      borderRadius="md"
-      boxShadow="md"
-      p={4}
-      spacing={4}
+    <VStack
+      color="black"
       backgroundColor="white"
+      cursor="pointer"
+      borderRadius="xl"
     >
-      <Image src={imageSrc} alt={title} width={64} height={64} borderRadius="md"/>   
-      <VStack align="flex-start" spacing={2} flex={1}>
-        <Heading as="h2" size="md" fontWeight="bold" color="black">
-          {title}
-        </Heading>
-        <Text color="black">{description}</Text>
+      <Image src={imageSrc} alt={title} borderRadius="xl" />
+      <VStack spacing={4} alignItems="flex-start" p={5}>
+        <Heading as="h4" size="md">{title}</Heading>
+        <Text color="#64748b" fontSize="lg">{description}</Text>
+        <HStack fontSize="md" fontWeight="medium">
+          <p>See more</p>
+          <FontAwesomeIcon icon={faArrowRight} size="1x" />
+        </HStack>
       </VStack>
-      <FontAwesomeIcon icon={faArrowRight} size="1x"/>
-    </HStack>
+    </VStack>
   )
-  
+
 };
 
 export default Card;
--- ReactCoursera/PortfolioApp/src/components/ContactMeSection.js
+++ ReactCoursera/PortfolioApp/src/components/ContactMeSection.js
@@ -17,46 +17,38 @@ import FullScreenSection from "./FullScreenSection";
 import useSubmit from "../hooks/useSubmit";
 import { useAlertContext } from "../context/alertContext";
 
-const LandingSection = () => {
+const ContactMeSection = () => {
   const { isLoading, response, submit } = useSubmit();
   const { onOpen } = useAlertContext();
 
   const formik = useFormik({
+    // Form validation
     initialValues: {
       firstName: "",
       email: "",
       type: "hireMe",
-      comment: "",
+      comment: ""
     },
     onSubmit: (values) => {
-      submit(values).then(() => {
-        if (response.status === "success") {
-          formik.resetForm();
-          onOpen("success", "Form submitted successfully!")
-        } else {
-          onOpen("error", "Failed to submit the form. Please try again later.")
-        }
-
-      })
+      submit("", values);
     },
     validationSchema: Yup.object({
       firstName: Yup.string().required("Required"),
-      email: Yup.string()
-        .email("Invalid email address.")
-        .required("Required"),
-      comment: Yup.string().required("Required").min(25, "Must be at least 25 characters"),
-    }),
+      email: Yup.string().email("Invalid email address").required("Required"),
+      comment: Yup.string().min(25, "Must be at least 25 characters").required("Required")
+    })
   });
+
+  // Show an alert when the form is submitted successfully
   useEffect(() => {
     if (response) {
-      if (response.type === "success") {
-        onOpen(response.message); // Display success alert with the message
-        formik.resetForm(); // Reset the form
-      } else if (response.type === "error") {
-        onOpen(response.message); // Display error alert with the message
-      }
+      onOpen(response.type, response.message);
+      // Reset the form if the response is successful
+      if (response.type === "success")
+        formik.resetForm();
     }
   }, [response]);
+
   return (
     <FullScreenSection
       isDarkBackground
@@ -69,34 +61,38 @@ const LandingSection = () => {
           Contact me
         </Heading>
         <Box p={6} rounded="md" w="100%">
+          {/* Connect the form onSubmit prop with Formik's handleSubmit function */}
           <form onSubmit={formik.handleSubmit}>
             <VStack spacing={4}>
-              <FormControl 
-                isInvalid={formik.touched.firstName && formik.errors.firstName}>
+              {/* Show the error messages for each field when the field is touched and the validation fails */}
+              <FormControl isInvalid={!!formik.errors.firstName && formik.touched.firstName}>
                 <FormLabel htmlFor="firstName">Name</FormLabel>
                 <Input
                   id="firstName"
                   name="firstName"
-                  {...formik.getFieldProps("firsName")}
+                  {...formik.getFieldProps("firstName")} // Make the Input components from Chakra UI controlled components
                 />
+                {/* Show the error messages for each field when the field is touched and the validation fails */}
                 <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
               </FormControl>
-              <FormControl isInvalid={formik.touched.email && formik.errors.email}>
+              <FormControl isInvalid={!!formik.errors.email && formik.touched.email}>
+                {/* Show the error messages for each field when the field is touched and the validation fails */}
                 <FormLabel htmlFor="email">Email Address</FormLabel>
                 <Input
                   id="email"
                   name="email"
                   type="email"
-                  {...formik.getFieldProps("email")}
+                  {...formik.getFieldProps("email")} // Make the Input components from Chakra UI controlled components
                 />
+                {/* Show the error messages for each field when the field is touched and the validation fails */}
                 <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
               </FormControl>
               <FormControl>
                 <FormLabel htmlFor="type">Type of enquiry</FormLabel>
-                <Select
-                  id="type"
+                <Select 
+                  id="type" 
                   name="type"
-                  {...formik.getFieldProps("type")}
+                  {...formik.getFieldProps("type")} // Make the Input components from Chakra UI controlled components
                 >
                   <option value="hireMe">Freelance project proposal</option>
                   <option value="openSource">
@@ -105,7 +101,8 @@ const LandingSection = () => {
                   <option value="other">Other</option>
                 </Select>
               </FormControl>
-              <FormControl isInvalid={formik.touched.comment && formik.errors.comment}>
+              {/* Show the error messages for each field when the field is touched and the validation fails */}
+              <FormControl isInvalid={!!formik.errors.comment && formik.touched.comment}>
                 <FormLabel htmlFor="comment">Your message</FormLabel>
                 <Textarea
                   id="comment"
@@ -113,14 +110,11 @@ const LandingSection = () => {
                   height={250}
                   {...formik.getFieldProps("comment")}
                 />
+                {/* Show the error messages for each field when the field is touched and the validation fails */}
                 <FormErrorMessage>{formik.errors.comment}</FormErrorMessage>
               </FormControl>
-              <Button 
-                type="submit" 
-                colorScheme="purple" 
-                width="full"
-                isLoading={isLoading}
-                >
+              {/* Show a loading indicator */}
+              <Button type="submit" colorScheme="purple" width="full" isLoading={isLoading}>
                 Submit
               </Button>
             </VStack>
@@ -131,4 +125,4 @@ const LandingSection = () => {
   );
 };
 
-export default LandingSection;
+export default ContactMeSection;
